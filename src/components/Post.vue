4b26dca6cacd6eb7a9634b5e1174e85ca0a0315b
ZvoniFadic <zklennerf@gmail.com>
2021-08-13 11:08:27 -0400

    Version 1.1

---
+++ src/components/Post.vue
@@ -0,0 +1,27 @@
+<template>
+
+<div>
+    <!-- IMPLEMENTAR EL ROUTER VIEW QUE MUSTRE EL ARTICULO-->
+    
+<div>
+    
+    <router-view :to="{name: 'articulo'}"></router-view> 
+    
+   </div>
+
+ 
+</div>
+    
+</template>
+
+<script>
+export default {
+   
+ 
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Articulo from './components/Articulo.vue'
+import Contacto from './components/Contacto.vue'
+import Portada from './components/Portada.vue'
+import Post from './components/Post.vue'
+import SobreMi from './components/SobreMi.vue'
+import NotFound from './components/NotFound.vue'
+import { post } from 'jquery'
+
+Vue.use(Router)
+
+export default new Router({
+    node:'history',
+    routes:[
+        {
+            path:'/',
+            component:Portada,
+            name:'portada'
+        },
+        {
+            path:'/contacto',
+            component:Contacto,
+            name:'contacto'
+        },
+        {
+            path:'/sobremi',
+            component:SobreMi,
+            name:'sobremi'
+        },
+        {
+            path:'/post',
+            component:Post,
+            name:'post',
+            children:[
+                {
+                    path:':articulo',
+                    component: Articulo,
+                    name:'articulo'
+                },
+            ]
+            
+        },
+        {
+            path:'*',
+            component: NotFound,
+        },
+    ]
+})
